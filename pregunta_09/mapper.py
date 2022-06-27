ede2305ff43aeff92d806f7f5c319ed5b2689a0e
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 18:30:23 -0500

    finish

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for line in sys.stdin:
+
+    letter,date,value = line.split()
+    
+    sys.stdout.write("{}\t{}\t{}\n".format(value,date,letter))
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    list_of_list = []
+    for line in sys.stdin:
+        value,date,letter = line.split()
+        value = int(value)
+        list_of_list.append([value,date,letter])
+    list_of_list = sorted(list_of_list,key=lambda x:x[0],reverse=False)
+    i = 0
+    for list in list_of_list:
+        i += 1
+        if i > 6:
+            break
+        else: 
+            sys.stdout.write(list[2] + "   " + list[1] + "   " + str(list[0]) + "\n")
+
+
