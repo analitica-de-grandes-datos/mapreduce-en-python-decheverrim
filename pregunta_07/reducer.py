6c6b9708e052e06cc2fa8813a630599533ed74a2
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 16:19:38 -0500

    finish

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+
+
+if __name__ == "__main__":
+    campo_lista = []
+    for line in sys.stdin:
+        marca,date,value = line.split()
+        value = int(value)
+        campo_lista.append([marca,date,value])
+
+    campo_lista = sorted(campo_lista,key=lambda x:(x[0],x[2]))
+    for list_ in campo_lista:
+        sys.stdout.write(list_[0] +"   " + list_[1] +"   " + str(list_[2]) + "\n")
+    
+
+
+        
