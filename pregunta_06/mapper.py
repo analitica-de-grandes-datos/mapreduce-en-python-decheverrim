ed9d1426a7e9ea1a118faebcc956a1b8401f2f98
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 06:16:50 -0500

    finish

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        sys.stdout.write("{}\t{}\n".format(line.split()[0],float(line.split()[2])))
+        
