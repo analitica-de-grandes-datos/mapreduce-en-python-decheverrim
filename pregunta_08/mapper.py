406508334e9b7d0ccd9a7283f64505d8a9be9453
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 16:46:30 -0500

    finish

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        key = line.split()[0]
+        value = line.split()[2]
+        sys.stdout.write("{}\t{}\n".format(key,value))
+        
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+    
+    
+    
+    curkey = None
+    list_values = []
+    for line in sys.stdin:
+        key, value = line.split("\t")
+        value = float(value)
+        if key == curkey:
+            list_values.append(value)
+        elif curkey is None:
+            curkey = key
+            list_values.append(value)
+        else:
+            sys.stdout.write("{}\t{}\t{}\n".format(curkey,sum(list_values),(sum(list_values)/len(list_values))))
+            list_values = []
+            list_values.append(value)
+            curkey = key
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey,sum(list_values),(sum(list_values)/len(list_values))))
