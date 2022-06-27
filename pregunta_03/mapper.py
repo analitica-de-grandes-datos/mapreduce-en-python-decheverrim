f6dc93d732cdcd7f91ec7015eeb4394e1c71c167
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 05:07:16 -0500

    finish

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    
+
+
+    for line in sys.stdin:
+        key = line.split(',')[0]
+        value = int(line.split(',')[1])
+        sys.stdout.write("{}\t{}\n".format(value,key))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+
+
+    for line in sys.stdin:
+        key = line.split()[1]
+        value = int(line.split()[0])
+        sys.stdout.write("{},{}\n".format(key, value))
+            
+
