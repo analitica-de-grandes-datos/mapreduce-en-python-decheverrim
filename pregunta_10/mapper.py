ab99bbd6243258a8f40a7849a596e8f90ce382a6
David Echeverri Montoya <95106005+decheverrim@users.noreply.github.com>
2022-06-27 19:24:18 -0500

    finish

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        value = line.split()[0]
+        keys = line.split()[1]
+        list_keys = keys.split(",")
+        for list_ in list_keys:
+            sys.stdout.write("{}\t{}\n".format(list_[0],value))
+    
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    
+    
+    
+    curkey = None
+    list_values = []
+    for line in sys.stdin:
+        key, value = line.split()
+        value = int(value)
+        if key == curkey:
+            list_values.append(value)
+        elif curkey is None:
+            curkey = key
+            list_values.append(value)
+        else:
+            list_values_sorted = sorted(list_values)
+            list_vales_tostr = list(map(lambda element:str(element),list_values_sorted))         
+            sys.stdout.write("{}\t{}\n".format(curkey,",".join(list_vales_tostr)))
+            list_values = []
+            list_values.append(value)
+            curkey = key 
+    list_values_sorted = sorted(list_values)
+    list_vales_tostr = list(map(lambda element:str(element),list_values_sorted))         
+    sys.stdout.write("{}\t{}\n".format(curkey,",".join(list_vales_tostr)))    
